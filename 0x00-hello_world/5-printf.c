e267b177ef270e942d8f1d3d2956a974dadb8e2a
silas-inegbe <kingssilas18@gmail.co>
2022-10-14 03:35:01 -0700

    git add .

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point*
+* main - Entry point
 * Return: always 0
 */
 int main(void)
 {
-		puts("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
+
