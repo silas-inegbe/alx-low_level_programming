d3e0f4cc0f066a13a4c61e2a611c5089bface014
silas-inegbe <kingssilas18@gmail.co>
2022-10-14 03:13:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: always 0
- *
- *     */
+ * main - Entry point
+ * Return: always 0
+ */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
