5d575939760bfb78ba5b4ffea27bae76f1895902
silas-inegbe <kingssilas18@gmail.co>
2022-10-14 04:41:49 -0700

    ughh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	printf("Size of a char:1 %i byte(s)\n", sizeof(char));
-	printf("Size of an int:4 %d byte(s)\n", sizeof(int));
+	printf("Size of an int:4 %i byte(s)\n", sizeof(int));
 	printf("Size of a long int:4 %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int:8 %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float:4 %i byte(s)\n", sizeof(float));
