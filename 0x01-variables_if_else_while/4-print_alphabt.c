17313be15e0891598d8eebbfe0aceab879eb7a34
silas-inegbe <kingssilas18@gmail.co>
2022-10-14 16:43:37 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: 0
+ */
+init main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+
+	putchar('\n')
+
+	return (0);
+}
+
