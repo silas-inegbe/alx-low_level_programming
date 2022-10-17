4187fa51c2af29ec1f2ac8511b525f4da3dfbd53
silas-inegbe <kingssilas18@gmail.co>
2022-10-17 03:03:33 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,11 @@ int main(void)
 {
 	int num;
 	
-	for (num + 0; num < 10; num++)
-	printf("%d", num);
-
-	printf("\n");
+	for (num = 48; num < 58; num++)
+	{
+		printf("%d", num);
+	}
+	printf('\n');
 
 	return (0);
 }
