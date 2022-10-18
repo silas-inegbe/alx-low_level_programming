79381feefddd41192b6ac2ff088793de11791e88
silas-inegbe <kingssilas18@gmail.co>
2022-10-18 10:02:49 -0700

    fixed it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 
 	while (count++ <= 9)
 	{
-		for (letters = 'a'; letters <+ 'z'; letters++)
+		for (letters = 'a'; letters <= 'z'; letters++)
 			_putchar(letters);
 		_putchar('\n');
 	}
