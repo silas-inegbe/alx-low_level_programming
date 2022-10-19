1a0648cfac3c3db084cdbf466afb0ae85459cb46
silas-inegbe <kingssilas18@gmail.co>
2022-10-19 05:47:40 -0700

    printer integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,12 +1,23 @@
 #include "main.h"
  /**
-  * _abs - a function that coputes absolute value of an integer
-  * @n: integer input 
+  * _abs - Entry point
+  * @n: n is an integer
   *
-  * Description: new function
-  * Return: Absolute value of ab
+  * Description: prints the absolute value of an integer
+  * Return: int
   */
 int _abs(int n)
 {
-	return (n * ((n > 0) - (n < 0)));
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
 }
