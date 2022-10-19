2b50f9d54014da767f716985bced7f8fcfbf31e7
silas-inegbe <kingssilas18@gmail.co>
2022-10-19 03:30:45 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - a function that prints the sign of a number
+ * @n: The inpyt number as an integer.
+ *
+ * Return: 1 is greater than zero. 0 is zero -1 is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar (43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+	_putchar('\n')
+}
+
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,4 @@ Learning how to use functions and nested loops
 Task -1 Write a program that prints _putchar, followed by a new line
 Task -1 Write a function that prints the alphabet, in lowercase, followed by a new line.
 Task -2 Write a function that prints 10 times the alphabet, in lowercase, followed by a new line.
+Task -5 Write a function that prints the sign of a number.
