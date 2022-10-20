78e3759e3c7c915a11d052a7b3e113660da27efa
silas-inegbe <kingssilas18@gmail.co>
2022-10-19 20:07:11 -0700

    12. The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_times_table - Prints a multiplication table up to date
  * @n: integer
@@ -6,6 +7,7 @@
 void print_times_table(int n)
 {
 	int i, j, k;
+
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
