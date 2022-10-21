91fe8c6699ed4f70d20aa9c8c2bcfa1394844a03
silas-inegbe <kingssilas18@gmail.co>
2022-10-21 08:33:17 -0700

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line
+ *
+ *Return: void
+ */
+void print_numbers(void)
+{
+	char i;
+	for (i = '0'; i <= '9'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 project description 
 Task -2 Write a function that multiplies two integers.
+Task -3 Write a function that prints the numbers, from 0 to 9, followed by a new line.
