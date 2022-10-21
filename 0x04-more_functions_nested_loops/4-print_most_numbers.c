8df4116c41a272e430f9c4c570b8151a9f6e329c
silas-inegbe <kingssilas18@gmail.co>
2022-10-21 09:18:01 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,8 +8,12 @@ void print_most_numbers(void)
 {
 	int i;
 
-	for(i = '0'; i <= '9'; i++)
-		if (i !+ '2' && i = '4')
-			_putchar(i);
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i = '0');
+	}
 	_putchar('\n');
 }
