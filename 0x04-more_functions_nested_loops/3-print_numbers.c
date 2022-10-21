b92d5be2e93b0765bae62929224f7b16f71fab5e
silas-inegbe <kingssilas18@gmail.co>
2022-10-21 08:35:48 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,11 @@
 
 /**
  * print_numbers - a function that prints the numbers, from 0 to 9, followed by a new line
- *
  *Return: void
  */
 void print_numbers(void)
 {
+	
 	char i;
 	for (i = '0'; i <= '9'; i++)
 		_putchar(i);
