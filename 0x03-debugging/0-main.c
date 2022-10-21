256082b2a04cc0cc6b75d8008ed30157994d06c0
silas-inegbe <kingssilas18@gmail.co>
2022-10-21 11:14:04 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#inclue "main.h"
+
+/**
+ * main - tests function that prints if integer is + or -
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
