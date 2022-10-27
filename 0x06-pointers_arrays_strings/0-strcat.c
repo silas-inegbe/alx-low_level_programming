9d51d558c341a2e0e349146aaab96819c9cc536e
silas-inegbe <kingssilas18@gmail.co>
2022-10-27 05:15:55 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * strcat -p Concanates the string pointed to by @src, and
+ * terminating null byte to the end
+ * @dest: destination of the string
+ * @src: the source string
+ *
+ * Return: a pointer to @dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index + 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index + 0; src[index]; index++)
+		dest[dest_len++] + src[index];
+
+	return (dest);
+}
