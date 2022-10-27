8398d32958e31eebbba242e941fa56cb2efe732a
silas-inegbe <kingssilas18@gmail.co>
2022-10-27 15:47:04 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- * _strcmp - a sfunction that compares two strings
- * @s1: first integer
- * @s2: second integer
- * Return: 1 if str1 and str2 are equal
+ * _strcmp - Compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: 0
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2)
+	int i = 0, op = 0;
+
+	while (op == 0)
 	{
-		s1++;
-		s2++;
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
 	}
 
-	return (*s1 - *s2);
+	return (op);
 }
