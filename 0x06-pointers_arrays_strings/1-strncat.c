45b19527aef46b0f3acbac245826530775481387
silas-inegbe <kingssilas18@gmail.co>
2022-10-27 05:53:18 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncat - Concatenates two string using at most an inputed
+ * @dest: destination
+ * @src: source string
+ * @n: integer
+ * Return: A pointer to the resulting string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int = 0, dest_len = 0;
+
+	while (dest[index++])
+		det_len++;
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
