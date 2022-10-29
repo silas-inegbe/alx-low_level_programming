e33253baf7a03476e947f2819c69539293c88c3f
silas-inegbe <kingssilas18@gmail.co>
2022-10-28 18:23:16 -0700

    6-cap_string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * cap_string - Capitalises all words of a string
  * @str: string
@@ -6,31 +7,29 @@
  */
 char *cap_string(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1] == ' ' ||
-			str[i - 1] == '\t' ||
-			str[i - 1] == '\n' ||
-			str[i - 1] == ',' ||
-			str[i - 1] == ';' ||
-			str[i - 1] == '.' ||
-			str[i - 1] == '!' ||
-			str[i - 1] == '?' ||
-			str[i - 1] == '"' ||
-			str[i - 1] == '(' ||
-			str[i - 1] == ')' ||
-			str[i - 1] == '{' ||
-			str[i - 1] == '}' ||
-			i == 0)
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
+		i == 0)
 			str[i] -= 32;
 
-		i++
+		i++;
 	}
 	return (str);
 }
-
