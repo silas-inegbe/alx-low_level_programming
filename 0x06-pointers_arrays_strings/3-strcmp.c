39b254479b0c5a32ad4039f2ad46dbb1c479b086
silas-inegbe <kingssilas18@gmail.co>
2022-10-28 17:20:26 -0700

    3-strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,19 +4,21 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: 0
+ * Return: 0 if equal, other wise the difference
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, op = 0;
+	int i;
 
-	while (op == 0)
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 		i++;
 	}
 
-	return (op);
+	return (0);
 }
