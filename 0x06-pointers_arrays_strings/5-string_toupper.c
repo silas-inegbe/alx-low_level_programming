7e1754badee942cbacbfb4cf971721c3588a0547
silas-inegbe <kingssilas18@gmail.co>
2022-10-28 17:45:03 -0700

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper \- changes lower to uppercase
+ * @n: pointer
+ *
+ * Return: char
+ */
+char *string_toupper(char *)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
