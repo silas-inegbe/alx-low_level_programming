9d9b19d6e994551caba0d94fff23f48f02919c08
silas-inegbe <kingssilas18@gmail.co>
2022-11-08 08:42:12 -0800

    yh

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,34 +11,34 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int *twoD;
-	int hgt_index, wid_index;
+	int **grid;
+	int i, j;
 
 	if (width <= 0 || height <= 0)
-		return (NULL);
-
-	twoD = malloc(sizeof(int *) * height);
-
-	if (twoD == NULL)
-		return (NULL);
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		twoD[hgt_index] = malloc(sizeof(int) * width);
-		if (twoD[hgt_index] == NULL)
-		{
-			for (; hgt_index >= 0; hgt_index--)
-				free(twoD[hgt_index]);
+		return (NULL);
+	}
 
-			free(twoD);
-			return (NULL);
+	grid = malloc(sizeof(int *) * height);
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (i = i - 1; i >= 0; i--)
+			{
+				free(grid[i]);
+			}
+				free(grid);
+				return (NULL);
 		}
 	}
-
-	for (hgt_index = 0; hgt_index < height; hgt_index++)
-	{
-		for (wid_index = 0; wid_index < width; wid_index++)
-		twoD[hgt_index][wid_index] = 0;
-	}
-
-	return (twoD);
+		for (i = 0; j < width; j++)
+			grid[i][j] = 0;
+		return (grid);
 }
