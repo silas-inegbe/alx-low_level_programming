2734b684458fcbdf04cd8940e2a1ce95d1ad37ab
silas-inegbe <kingssilas18@gmail.co>
2022-11-11 04:49:38 -0800

    done

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,7 +23,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (index = 0; s1[index]; index++)
 		len++;
 
-	concat = malloc(sizeof(char) * (len + 1))
+	concat = malloc(sizeof(char) * (len + 1));
 
 		if (concat == NULL)
 			return (NULL);
