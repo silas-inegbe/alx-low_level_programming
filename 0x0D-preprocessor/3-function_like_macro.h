4bacfa3212296bf4f8e9b4acea440ed67f2c6ebc
silas-inegbe <kingssilas18@gmail.co>
2022-11-14 06:56:57 -0800

    completed

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * Desc: computes the absolute value of a number x
+ */
+
+#define ABS(x) ((x) < 0 ? (-x) : (x))
+
+#endif
