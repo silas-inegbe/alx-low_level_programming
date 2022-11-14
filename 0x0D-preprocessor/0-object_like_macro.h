a573837ad9b0cad13cd4133049a5985bbe31c26c
silas-inegbe <kingssilas18@gmail.co>
2022-11-14 02:08:33 -0800

    completed task

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+
+#define SIZE 1024
+
+#endif
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,6 +1,6 @@
 # 0x0D. C - Preprocessor
 
-* 0-object_like_macro.h
+* (./0-object_like_macro.h)
 
 * 1-pi.h
 
