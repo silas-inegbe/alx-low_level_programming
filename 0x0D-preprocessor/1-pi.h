c2dd6ffdd3c4b1c9d77c1241b474e9ed50746d4c
silas-inegbe <kingssilas18@gmail.co>
2022-11-14 06:13:36 -0800

    complete

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,10 @@
 #ifndef PI_H
 #define PI_H
 
-/** File: 1-pi.h
- * Desc: a header file that defines a macro named PI
- * as an abbreviation for the token 3.14159265359
+/**
+ * main - entry point
+ *
+ * Return: 0
  */
 
 #define PI 3.14159265359
