4d788f485a773a1d50622bd8452992283e5e82df
silas-inegbe <kingssilas18@gmail.co>
2022-11-14 05:12:21 -0800

    complete

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was compiled from,
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
