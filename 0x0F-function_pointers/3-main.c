bc815e30f2be9bf0acbbb009839e0f50aa5b5314
silas-inegbe <kingssilas18@gmail.co>
2022-11-16 15:36:31 -0800

    almost done

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,12 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - prints the result of operations.
+ * @argc: number of arguments
+ * @argv: array of pointers
+ *
+ * Return: Always 0
+ */
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,3 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
