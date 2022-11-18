facf1217612e9a5ae6b136fbf60bedde8bf3e5f7
silas-inegbe <kingssilas18@gmail.co>
2022-11-17 23:20:36 -0800

    done

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,7 +2,7 @@
 
 /**
  * print_name - Prints name
- * @name Name to be printed
+ * @name: Name to be printed
  * @f: A pointer to a function that prints a name
  */
 void print_name(char *name, void (*f)(char *))
