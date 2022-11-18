e8f532f25153cd8b0007cdbcec10c4837ca5dd0e
silas-inegbe <kingssilas18@gmail.co>
2022-11-17 23:07:10 -0800

    almost

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(strings, char *);
 
 		if (str == NULL)
-			printf("(nill)");
+			printf("(nil)");
 		else
 			printf("%s", str);
 
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,5 +4,41 @@
 
 /**
  * print_all - A function that prints anything
- *
+ * @format: a list of types of arguments passed to the function
+ * @...: Arguments passed to function
+ */
 void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0; j = 0;
+	char *separator = "";
+
+	printer_t funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format && (*(format + i)))
+	{
+		j = 0;
+
+		while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print(args);
+			separator = ", ";
+		}
+
+		i++;
+	}
+	printf("\n");
+
+	va_end(args);
+}
