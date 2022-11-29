6e35ee5186515f102ddee7f3eb85e140af637027
silas-inegbe <kingssilas18@gmail.co>
2022-11-29 14:23:34 -0800

    complete

--- 0x13-more_singly_linked_lists/_putchar.c
+++ 0x13-more_singly_linked_lists/_putchar.c
@@ -4,22 +4,18 @@
 
 /**
  *
- *  * _putchar - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  *
- *   * @c: The character to print
+ * @c: The character to print
  *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
 int _putchar(char c)
 
 {
 
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 
 }
