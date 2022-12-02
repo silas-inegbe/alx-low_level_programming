8979fb3cb8e4a5cef0162c59eb981e79fcd85642
silas-inegbe <kingssilas18@gmail.co>
2022-12-01 20:35:33 -0800

    ok

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -15,16 +15,11 @@ unsigned int binary_to_uint(const char *b)
 	if (b[len] == '\0')
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while ((b[len] == '0') || (b[len] == '1'))
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
 	}
 
 	return (num);
